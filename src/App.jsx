423a20093bedbd168e229301a81a147adc61d8ac
DilipBsr <dilipkr.bs1999@gmail.com>
2024-09-10 16:41:47 +0530

    tell to harsh

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './component/Navbar'
 import Home from './component/Home'
 import Card from './component/Card'
+import Payment from './component/Payment'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { dark } from '@mui/material/styles/createPalette'
 import DarkTheme from './Theme/DarkTheme'
@@ -12,8 +13,7 @@ function App() {
   return (
     <ThemeProvider theme={DarkTheme}>
       <CssBaseline/>
-      <Navbar/>
-      <Home/>
+      <Payment/>
     </ThemeProvider>
   )
 }
